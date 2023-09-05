7688e3435725d5372ada774dbccf33934861df01
robinluo <luob@uniseas.com.cn>
2023-09-05 19:59:22 +0800

    feat: add logger middleware

--- createStore.js
+++ createStore.js
@@ -1,4 +1,10 @@
-function createStore(reducer) {
+function createStore(reducer, enhancer) {
+
+  // 当传入enhancer时，增强 store
+  if (enhancer) {
+    return enhancer(createStore)(reducer)
+  }
+
   let state = null
   let listeners = []
 
@@ -36,6 +42,55 @@ function createStore(reducer) {
 
 // 测试用例
 
+// applyMiddleware(a, b, c ...)
+// createStore(reducer, applyMiddleware(a, b, c ...))
+// 类似高阶函数写法
+function applyMiddleware(...middlewares) {
+  return (createStore) => (reducer) => {
+    const store = createStore(reducer)
+
+    const middlewareAPI = {
+      getState: store.getState,
+      dispatch: (action, ...args) => store.dispatch(action, ...args)
+    }
+
+    // 中间件函数的执行链
+    const middlewareChain = middlewares.map(middleware => middleware(middlewareAPI))
+
+    const dispatch = compose(...middlewareChain)(store.dispatch)
+
+    return {
+      ...store,
+      dispatch
+    }
+  }
+}
+
+// 通过中间件 middleware，来增强 dispatch 的功能
+function middleware(dispatch, getState) {
+  return (next) => {
+    return (action) => {
+      // 下一个中间件，实现中间件的串行调用
+      next(action)
+    }
+  }
+}
+
+function compose(...funcs) {
+  if (funcs?.length === 0) {
+    // 没有中间件
+    return (args) => args
+  }
+
+  if (funcs?.length === 1) {
+    // 只有一个中间件
+    return funcs[0]
+  }
+
+  // 多个中间件
+  return funcs.reduce((pre, next) => (...args) => pre(next(...args)))
+}
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'inc':
@@ -47,7 +102,7 @@ const reducer = (state, action) => {
   }
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger))
 
 const { subscribe, dispatch, getState } = store
 
@@ -59,3 +114,17 @@ subscribe(() => {
 dispatch({
   type: 'inc'
 })
+
+function logger({ dispatch, getState }) {
+  return next => action => {
+    const preState = getState()
+    console.log('start logger');
+    console.log('pre state', preState)
+    console.log('action', action);
+    const result = next(action)
+    const nextState = getState()
+    console.log('next state', nextState)
+
+    return result
+  }
+}
